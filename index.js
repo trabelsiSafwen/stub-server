f3eea8b31fe4825023305a1f459591104ca7ed0d
Safouane Trabelsi <safwen.trabelsi@neofacto.com>
2021-02-18 17:22:33 +0100

    WIP

--- index.js
+++ index.js
@@ -8,17 +8,26 @@ var serverPort = 8080;
 
 // swaggerRouter configuration
 var options = {
-    routing: {
-        controllers: path.join(__dirname, './controllers')
-    },
+	routing: {
+		controllers: path.join(__dirname, './controllers'),
+	},
 };
 
-var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
+var expressAppConfig = oas3Tools.expressAppConfig(
+	path.join(__dirname, 'api/openapi.yaml'),
+	options
+);
 var app = expressAppConfig.getApp();
 
 // Initialize the Swagger middleware
-http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+http.createServer(app).listen(process.env.PORT, function () {
+	console.log(
+		'Your server is listening on port %d (http://localhost:%d)',
+		serverPort,
+		serverPort
+	);
+	console.log(
+		'Swagger-ui is available on http://localhost:%d/docs',
+		serverPort
+	);
 });
-
